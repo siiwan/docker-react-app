55b2a3c0c14e88296c2dc318124b3cdea8ddb7e6
gsw <ggssww99@gmail.com>
2023-12-03 13:38:45 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          안녕하세요 반갑습니다.!!!
         </a>
       </header>
     </div>
